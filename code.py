553cc5d1dabd3aab99ab2a1fe0cf482002ffe5c6
wasay1432 <kiyaniabdulwasay@gmail.com>
2024-07-12 05:32:02 -0700

    Diabetes.py

--- code.py
+++ code.py
@@ -1,42 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import scipy as sp
 
 file = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/diabetes.csv')
 
 a = file.head(10)
 
 age = a['Age']
-glu = a['Glucose']
+glucose = a['Glucose']
 
-i = True
-while i == True:
 
-    def glucose():
-        plt.plot(age, glu, marker='o',linestyle='-', color='b')
-        plt.title('')
-        plt.xlabel('Age')
-        plt.ylabel('Glucose Level')
-        plt.grid(True)
-        plt.show()
-        return glucose
+def f1():
+    plt.plot(age, glucose, marker='o',linestyle='-', color='b')
+    plt.title('')
+    plt.xlabel('Age')
+    plt.ylabel('Glucose Level')
+    plt.grid(True)
+    plt.show()
+    return glucose
 
-    def bp():
-        a.BloodPressure.value_counts().plot(kind='density')
-        plt.title("Blood Pressue")
-        plt.grid(True)
-        plt.show()
-        return bp
+def f2():
+    a.BloodPressure.value_counts().plot(kind='density')
+    plt.title("Blood Pressue")
+    plt.show()
+    return bp
+        
+print(f1())
+print(f2())
 
-    inp = input("1: Glucose\n2: Blood Pressure\n3: Quit\nEnter the value: ")
-    if inp == "1":
-        print(glucose())
-
-    elif inp == "2":
-        print(bp())
-
-    elif inp == "3":
-        break
-
-    else:
-        print("Enter the Number (1 to 3)")
+    
